1067e48d9e5cf34563f72c8f4965eb4138088ab5
Adrian Castro <laughingmandev@gmail.com>
2026-05-12 03:36:31 +0000

    Generador completo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ['su', 'mi', 'tu', 'el'];
+  let noun = ['mapache', 'tlacuache', 'raton', 'novio'];
+  let adj = ['mugroso', 'tuerto', 'fumado', 'tieso'];
+  let ext = ['.com', '.mx', '.gob', '.net'];
+
+  let listDom = '<ul>'; 
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < noun.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          let genDomain = (pronoun[i] + noun[j] + adj[k] + ext[l]);
+          listDom += `<li>${genDomain}</li>`;
+        }
+      }
+    }
+  }
+  listDom += '</ul>';
+  document.querySelector('#genDom').innerHTML = listDom;
 };
+
